5a7a8197dbc4f5305d11619f9a81528ab6ff01a3
Klevison Matias <klevison.matias@servicos.wf>
2017-01-04 08:51:30 -0300

    repositories module refactored and pull request view and viewmodel created

---
+++ desafio-ios/desafio-ios/Application/PullRequests/PullRequestViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PullRequestsViewController.swift
+//  desafio-ios
+//
+//  Created by Klevison Matias on 1/3/17.
+//  Copyright © 2017 Arena. All rights reserved.
+//
+
+import UIKit
+
+final class PullRequestsViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ desafio-ios/desafio-ios/Application/Repositories/RepositoriesTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoriesTableViewCell.swift
+//  desafio-ios
+//
+//  Created by Klevison Matias on 1/3/17.
+//  Copyright © 2017 Arena. All rights reserved.
+//
+
+import Foundation
